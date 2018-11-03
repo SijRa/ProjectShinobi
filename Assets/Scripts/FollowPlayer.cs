8c17a469e590a15cee3f7451d95e9ab39d6999d0
Sijan Shreesh Rana <Sijan_Rana@hotmail.com>
2018-11-03 21:14:30 +0000

    Second Checkpoint
    - Character sprite added (with animation)
    - Character movement added
    - Camera tracking added
    - Background, ship wreck added
    - Boxes added

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public GameObject PlayerObject;
+
+
+    public float xMin = -1.5f;
+    public float xMax = 1.5f;
+    public float yMin = -5.5f;
+    public float yMax = 5.5f;
+
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(PlayerObject.transform.position.x, PlayerObject.transform.position.y, -5);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector3 newPos = PlayerObject.transform.position;
+
+        if (newPos.x > xMin && newPos.x < xMax && newPos.y > yMin && newPos.y < yMax)
+        {
+            transform.position = new Vector3(newPos.x, newPos.y, -5);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float SwimSpeed = 10f;
+    SpriteRenderer sRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (sRenderer == null)
+        {
+            sRenderer = transform.GetComponent<SpriteRenderer>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            sRenderer.flipX = true;
+        }
+
+        if (!Input.GetKey(KeyCode.LeftArrow))
+        {
+            sRenderer.flipX = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.localPosition += Vector3.down * SwimSpeed * 0.5f;
+            transform.localRotation = Quaternion.Euler(0f, 0f, -90f);
+        }
+        else
+        {
+            transform.localPosition -= Vector3.down * SwimSpeed * 0.5f;
+            transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.Space))
+        {
+            transform.localPosition += Vector3.left * SwimSpeed * 0.25f;
+            transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+
+
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.Space))
+        {
+            transform.localPosition += Vector3.right * SwimSpeed * 0.25f;
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.Space))
+        {
+            transform.localPosition += Vector3.left * SwimSpeed * 0.25f;
+            transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.Space))
+        {
+            transform.localPosition += Vector3.right * SwimSpeed * 0.25f;
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+    }
+}
